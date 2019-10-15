f32302c8a10a299a359602f9013e3c46605abab5
johakr <Johannes.Kronmueller@gmail.com>
2019-10-15 11:44:20 +0200

    react example

---
+++ examples/08-react/webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
